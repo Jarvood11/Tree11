e0c9e3ab96c8ba0f245cef787ee0f6dc72ccc287
ewent <ewent@LAPTOP-23S3E4HC>
2023-12-23 23:24:19 +0300

    Добавьте файлы проекта.

---
+++ Tree11/Tree11.cpp
@@ -0,0 +1,75 @@
+﻿// Tree11.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+/*
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+*/
+
+// Binary Tree in C++
+#include <stdlib.h>
+#include <iostream>
+using namespace std;
+struct node {
+    int data;
+    struct node* left;
+    struct node* right;
+};
+// New node creation
+struct node* newNode(int data) {
+    struct node* node = (struct node*)malloc(sizeof(struct node));
+    node->data = data;
+    node->left = NULL;
+    node->right = NULL;
+    return (node);
+}
+// Traverse Preorder
+void traversePreOrder(struct node* temp) {
+    if (temp != NULL) {
+        cout << " " << temp->data;
+        traversePreOrder(temp->left);
+        traversePreOrder(temp->right);
+    }
+}
+// Traverse Inorder
+void traverseInOrder(struct node* temp) {
+    if (temp != NULL) {
+        traverseInOrder(temp->left);
+        cout << " " << temp->data;
+        traverseInOrder(temp->right);
+    }
+}
+// Traverse Postorder
+void traversePostOrder(struct node* temp) {
+    if (temp != NULL) {
+        traversePostOrder(temp->left);
+        traversePostOrder(temp->right);
+        cout << " " << temp->data;
+    }
+}
+int main() {
+    struct node* root = newNode(1);
+    root->left = newNode(2);
+    root->right = newNode(3);
+    root->left->left = newNode(4);
+    cout << "preorder traversal: ";
+    traversePreOrder(root);
+    cout << "\nInorder traversal: ";
+    traverseInOrder(root);
+    cout << "\nPostorder traversal: ";
+    traversePostOrder(root);
+}
+
+// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
+// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
+
+// Советы по началу работы 
+//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
+//   2. В окне Team Explorer можно подключиться к системе управления версиями.
+//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
+//   4. В окне "Список ошибок" можно просматривать ошибки.
+//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
+//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
